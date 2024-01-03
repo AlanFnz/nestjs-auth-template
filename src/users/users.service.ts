5d2a9e74c63e79f55b8154943c0d4074027c274f
Alan Fernandez <alan.f@msn.com>
2024-01-03 12:39:31 -0300

    users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async create(registerUserDto: RegisterUserDto): Promise<User> {
+    const { username, password } = registerUserDto;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = new User();
+    user.username = username;
+    user.password = hashedPassword;
+
+    return this.userRepository.save(user);
+  }
+
+  async findOne(id: number): Promise<User> {
+    return this.userRepository.findOne({ where: { id } });
+  }
+
+  async findByUsername(username: string): Promise<User> {
+    return this.userRepository.findOne({ where: { username } });
+  }
+}
