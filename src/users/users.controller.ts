b9b379adacee30245e43140755f9f5966cbad6a2
Alan Fernandez <alan.f@msn.com>
2024-01-15 12:11:45 -0300

    implemented services in controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Param, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { User } from './user.entity';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async createUser(@Body() registerUserDto: RegisterUserDto): Promise<User> {
+    return this.usersService.create(registerUserDto);
+  }
+
+  @Get(':id')
+  async findUserById(@Param('id') id: number): Promise<User> {
+    return this.usersService.findOne(id);
+  }
+
+  @Get('username/:username')
+  async findUserByUsername(@Param('username') username: string): Promise<User> {
+    return this.usersService.findByUsername(username);
+  }
+}
