4a9d1639aca2024c695095c724ee38b5384e10ea
Alan Fernandez <alan.f@msn.com>
2024-01-15 12:30:02 -0300

    improved error handling

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,24 +1,61 @@
-import { Controller, Post, Get, Param, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Param,
+  Body,
+  NotFoundException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { User } from './user.entity';
+import { ErrorResponse } from 'src/types';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  async createUser(@Body() registerUserDto: RegisterUserDto): Promise<User> {
-    return this.usersService.create(registerUserDto);
+  async createUser(
+    @Body() registerUserDto: RegisterUserDto,
+  ): Promise<User | ErrorResponse> {
+    try {
+      return await this.usersService.create(registerUserDto);
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        return { message: 'Username already exists', statusCode: 400 };
+      } else {
+        throw new InternalServerErrorException('Internal server error');
+      }
+    }
   }
 
   @Get(':id')
-  async findUserById(@Param('id') id: number): Promise<User> {
-    return this.usersService.findOne(id);
+  async findUserById(@Param('id') id: number): Promise<User | ErrorResponse> {
+    try {
+      return await this.usersService.findOne(id);
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        return { message: 'User not found', statusCode: 404 };
+      } else {
+        throw new InternalServerErrorException('Internal server error');
+      }
+    }
   }
 
   @Get('username/:username')
-  async findUserByUsername(@Param('username') username: string): Promise<User> {
-    return this.usersService.findByUsername(username);
+  async findUserByUsername(
+    @Param('username') username: string,
+  ): Promise<User | ErrorResponse> {
+    try {
+      return await this.usersService.findByUsername(username);
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        return { message: 'User not found', statusCode: 404 };
+      } else {
+        throw new InternalServerErrorException('Internal server error');
+      }
+    }
   }
 }
