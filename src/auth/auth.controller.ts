586c410f276f151c7ffa3329dac9b172a28b85ae
Alan Fernandez <alan.f@msn.com>
2024-01-18 20:14:41 -0300

    added sign in service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersService } from '../users/users.service';
 import { RegisterUserDto } from '../users/dto/register-user.dto';
+import { SignInDto } from './dto/sign-in.dto';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -10,8 +12,15 @@ export class AuthController {
     private readonly usersService: UsersService,
   ) {}
 
+  @Public()
   @Post('sign-up')
   async signUp(@Body() registerUserDto: RegisterUserDto) {
     return this.usersService.create(registerUserDto);
+  }
+
+  @Public()
+  @Post('sign-in')
+  async signIn(@Body() signInDto: SignInDto) {
+    return this.authService.signIn(signInDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
 import * as bcrypt from 'bcrypt';
+import { SignInDto } from './dto/sign-in.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  async validatePassword(username: string, password: string): Promise<boolean> {
-    const user = await this.usersService.findByUsername(username);
-    if (user && user.password) {
-      return bcrypt.compare(password, user.password);
+  async validatePassword(
+    userPassword: string,
+    password: string,
+  ): Promise<boolean> {
+    if (userPassword && password) {
+      return bcrypt.compare(password, userPassword);
     }
     return false;
+  }
+
+  async signIn(signInDto: SignInDto) {
+    const { username, password } = signInDto;
+
+    const user = await this.usersService.findByUsername(username);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid username or password');
+    }
+
+    const passwordIsValid = await this.validatePassword(
+      password,
+      user.password,
+    );
+
+    if (!passwordIsValid) {
+      throw new UnauthorizedException('Invalid username or password');
+    }
+
+    const payload = { sub: user.id, username: user.username };
+    const accessToken = await this.jwtService.signAsync(payload);
+
+    return { access_token: accessToken };
   }
 }
