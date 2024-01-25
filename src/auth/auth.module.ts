f927e648593005be6b4919f3be93c7184f218f9c
Alan Fernandez <alan.f@msn.com>
2024-01-25 19:25:46 -0300

    updated providers and imports on auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,17 +5,28 @@ import { JwtStrategy } from './strategy/jwt.strategy';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { RefreshTokenIdsStorage } from './refresh-token-ids-storage';
+import { LocalStrategy } from './strategy/local.strategy';
+import { JwtRefreshTokenStrategy } from './strategy/jwt-refresh-token.strategy';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
+    ConfigModule.forRoot(),
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'secret',
       signOptions: { expiresIn: '1h' },
     }),
   ],
-  providers: [AuthService, JwtStrategy],
+  providers: [
+    AuthService,
+    JwtStrategy,
+    RefreshTokenIdsStorage,
+    LocalStrategy,
+    JwtRefreshTokenStrategy,
+  ],
   exports: [AuthService],
   controllers: [AuthController],
 })
